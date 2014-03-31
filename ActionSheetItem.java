e5cb09a4ba3242e161d0ffb412d7743aea10c02e
Jason Hsu <support@tuoxie.me>
2014-03-31 15:24:51 +0800

    remove static

---
+++ ActionSheetItem.java
@@ -0,0 +1,15 @@
+package me.tuoxie.daai.ActionSheet;
+
+/**
+ * Created by jason on 14-3-31.
+ */
+public class ActionSheetItem {
+
+    String title;
+    ActionSheet.ActionSheetItemOnClickListener itemOnClickListener;
+
+    public ActionSheetItem(String title, ActionSheet.ActionSheetItemOnClickListener itemOnClickListener) {
+        this.title = title;
+        this.itemOnClickListener = itemOnClickListener;
+    }
+}
